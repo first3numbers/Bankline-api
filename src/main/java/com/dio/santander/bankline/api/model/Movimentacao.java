35ae60c31b9f11c6df8f713a8da8a7501aa94773
first3numbers <marcelohac2016@gmail.com>
2022-05-05 19:33:06 -0300

    added swagger

--- src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
+++ src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
@@ -18,13 +18,16 @@ public class Movimentacao {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	@Column(mame = "data_hora")
+	@Column(name = "data_hora")
 	private LocalDateTime dataHora;
 	private String descricao;
 	private Double valor;
 
 	@Enumerated(EnumType.STRING)
 	private MovimentacaoTipo tipo;
+
+	@Column(name = "id_conta")
+	private Integer idConta;
 
 	public Integer getId() {
 		return id;
@@ -64,6 +67,14 @@ public class Movimentacao {
 
 	public void setTipo(MovimentacaoTipo tipo) {
 		this.tipo = tipo;
+	}
+
+	public Integer getIdConta() {
+		return idConta;
+	}
+
+	public void setIdConta(Integer idConta) {
+		this.idConta = idConta;
 	}
 
 }
